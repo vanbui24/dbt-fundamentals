b904ba13619d6241dad2e659889bdfaff2bfaa79
Van Bui <van.bui@cybercom.com>
2022-03-10 20:58:37 +0000

    added test to citibike

---
+++ target/compiled/jaffle_shop/models/staging/citibike/stg__citibike.yml/not_null_stg__citibike_bike_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from analytics.dbt_vbui.stg__citibike
+where bike_id is null
+
+
---
+++ target/compiled/jaffle_shop/models/staging/citibike/stg__citibike.yml/not_null_stg__citibike_trip_duration.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from analytics.dbt_vbui.stg__citibike
+where trip_duration is null
+
+
